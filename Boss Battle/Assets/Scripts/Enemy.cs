20077208c20474cd855cc04438b92c3e1057ea2b
MikeB14 <41544531+MikeB14@users.noreply.github.com>
2018-08-10 16:23:55 -0700

    Attack1 Started

---
+++ Boss Battle/Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int health;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Sword")
+        {
+            Debug.Log("Damage Taken");
+        }
+    }
+}
---
+++ Boss Battle/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class PlayerAttack : MonoBehaviour {
+
+
+    #region MyVariables
+    private Animator Anim;
+    public int AttackCount = 0;
+    public float Attack1Timer;
+    #endregion
+
+
+    IEnumerator ResetAttack1()
+    {
+        yield return new WaitForSeconds(Attack1Timer);
+        AttackCount = 0;
+        Anim.SetBool("Attack1", false);
+       // Anim.SetBool("Idle", true);
+
+    }
+	// Use this for initialization
+	void Start () {
+        Anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1") && AttackCount == 0)
+        {
+            Anim.SetBool("Attack1", true);
+           // Anim.SetBool("Idle", false);
+            AttackCount = 1;
+
+        }
+        
+        if(AttackCount == 1)
+        {
+            
+            StartCoroutine(ResetAttack1());
+
+        }
+	}
+
+}
