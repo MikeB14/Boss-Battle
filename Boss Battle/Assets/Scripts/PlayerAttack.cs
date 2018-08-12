f26526ea3be841eaf359db631ae7b6857ab2977f
MikeB14 <41544531+MikeB14@users.noreply.github.com>
2018-08-11 20:56:51 -0700

    UltimateAttackStarted

--- Boss Battle/Assets/Scripts/PlayerAttack.cs
+++ Boss Battle/Assets/Scripts/PlayerAttack.cs
@@ -6,43 +6,83 @@ using UnityEngine.Animations;
 public class PlayerAttack : MonoBehaviour {
 
 
-    #region MyVariables
+    #region RegAttacks
+    
     private Animator Anim;
-    public int AttackCount = 0;
+    public bool IsAttacking1 = false;
+    private int AttackCount = 0;
     public float Attack1Timer;
+    #endregion
+
+    #region SpecAttacks
+    private Rigidbody2D RB;
+    public float UltAttackTimer;
+    private int UltAttackCount = 0;
+    public int UltAttackMeter = 0;
+   // public float UltAttackSpeed;
     #endregion
 
 
     IEnumerator ResetAttack1()
     {
         yield return new WaitForSeconds(Attack1Timer);
+        //IsAttacking1 = false;
         AttackCount = 0;
         Anim.SetBool("Attack1", false);
        // Anim.SetBool("Idle", true);
 
     }
+
+    IEnumerator ResetUltAttack()
+    {
+       yield return new WaitForSeconds(UltAttackTimer);
+       UltAttackCount = 0;
+        Anim.SetBool("UltAttack", false);
+    }
+
+
 	// Use this for initialization
 	void Start () {
         Anim = GetComponent<Animator>();
+        RB = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        Attack();
+        UltimateAttack();
+    }
 
+    private void Attack()
+    {
         if (Input.GetButtonDown("Fire1") && AttackCount == 0)
         {
             Anim.SetBool("Attack1", true);
-           // Anim.SetBool("Idle", false);
             AttackCount = 1;
+            // Anim.SetBool("Idle", false);
+            // IsAttacking1 = true;
 
         }
-        
-        if(AttackCount == 1)
+
+        if (AttackCount == 1)
         {
-            
+            //Anim.SetBool("Attack1", false);
             StartCoroutine(ResetAttack1());
 
         }
-	}
+    }
 
+
+    void UltimateAttack()
+    {
+        if(Input.GetButtonDown("Fire2") && UltAttackCount == 0)
+        {
+           // RB.velocity = Vector2. * UltAttackSpeed;
+            Anim.SetBool("UltAttack", true);
+            UltAttackCount = 1;
+            StartCoroutine(ResetUltAttack());
+        }
+
+    }
 }
